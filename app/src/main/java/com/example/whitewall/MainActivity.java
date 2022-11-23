1e3c7126b09bee39c6224e030ea6e7848a5a458e
Sergio-Jurado <juradotrillosergio1207@gmail.com>
2022-11-23 11:50:55 +0100

    He realizado la pantalla inicial de la aplicación.

--- app/src/main/java/com/example/whitewall/MainActivity.java
+++ app/src/main/java/com/example/whitewall/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.whitewall;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button abrir;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        abrir = findViewById(R.id.btn_abrir);
+
+        abrir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), MainActivity2.class);
+                startActivityForResult(intent, 0);
+            }
+        });
     }
 }
